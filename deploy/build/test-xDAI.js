2d0b98046e5895180858530d3879fba1a05007ff
mbecker20 <becker.maxh@gmail.com>
2021-03-18 05:25:03 -0700

    update

--- deploy/build/test-xDAI.js
+++ deploy/build/test-xDAI.js
@@ -1,27 +1,30 @@
 "use strict";
-// import createMempoolManager from './helpers/mempoolManager'
-// import createOpportunityManager from './helpers/opportunityManager'
-// import { chain } from './xDaiConfig'
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mempoolManager_1 = __importDefault(require("./helpers/mempoolManager"));
+const opportunityManager_1 = __importDefault(require("./helpers/opportunityManager"));
+const xDaiConfig_1 = require("./xDaiConfig");
 const path = require('path');
-// require('dotenv').config({ path: path.resolve(process.env.ENVPATH) })
-console.log(process.env.ENVPATH);
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 console.log('starting');
-// const init = async () => {
-// 	let blockNumber = 0
-// 	const { shouldIgnoreNextBlock, updateMempoolBlock, pgaManagers, parseTransaction } = createMempoolManager(chain)
-// 	chain.web3.eth.subscribe('pendingTransactions').on('data', async function (transaction) {
-// 		parseTransaction(transaction)
-// 	})
-// 	const { createOpportunityBatchList, executeLoadedBatch, opportunityParams } = createOpportunityManager(pgaManagers, shouldIgnoreNextBlock, chain)
-// 	await createOpportunityBatchList()
-// 	console.log(opportunityParams.pairs)
-// 	chain.web3.eth.subscribe('newBlockHeaders').on('data', async (block) => {
-// 		//console.log(`Block Number ${block.number}`)
-// 		if (block.number > blockNumber) {
-// 			blockNumber = block.number
-// 			await updateMempoolBlock(blockNumber)
-// 		}
-// 		executeLoadedBatch()
-// 	})
-// }
-// init()
+const init = async () => {
+    let blockNumber = 0;
+    const { shouldIgnoreNextBlock, updateMempoolBlock, pgaManagers, parseTransaction } = mempoolManager_1.default(xDaiConfig_1.chain);
+    xDaiConfig_1.chain.web3.eth.subscribe('pendingTransactions').on('data', async function (transaction) {
+        parseTransaction(transaction);
+    });
+    const { createOpportunityBatchList, executeLoadedBatch, opportunityParams } = opportunityManager_1.default(pgaManagers, shouldIgnoreNextBlock, xDaiConfig_1.chain);
+    await createOpportunityBatchList();
+    console.log(opportunityParams.pairs);
+    xDaiConfig_1.chain.web3.eth.subscribe('newBlockHeaders').on('data', async (block) => {
+        //console.log(`Block Number ${block.number}`)
+        if (block.number > blockNumber) {
+            blockNumber = block.number;
+            await updateMempoolBlock(blockNumber);
+        }
+        executeLoadedBatch();
+    });
+};
+init();
